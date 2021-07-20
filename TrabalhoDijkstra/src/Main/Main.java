62b6161f1e402e437ab2fba85cb0ca1624edee87
rodri <rodri@192.168.0.155>
2021-07-20 11:35:55 -0300

    Adicionando função de cálculo de tempo.

--- TrabalhoDijkstra/src/Main/Main.java
+++ TrabalhoDijkstra/src/Main/Main.java
@@ -11,21 +11,31 @@ public class Main {
 		String file1 = "C:\\Users\\rodri\\OneDrive\\Documentos\\Material de Estudo\\APA\\Instâncias\\check_v5_s1.dat";
 		String file2 = "C:\\Users\\rodri\\OneDrive\\Documentos\\Material de Estudo\\APA\\Instâncias\\check_v5_s2.dat";
 		
-		Arquivo arquivo = new Arquivo(file1);
+		Arquivo arquivo = new Arquivo(file2);
 		
 		arquivo.ler();
 		
 		DijkstraVetor dijkstraVetor = new DijkstraVetor(arquivo.getGrafo());
 		
+		long inicioVetor = System.currentTimeMillis();
 		dijkstraVetor.calculaMenorCaminho(0);
+		long fimVetor = System.currentTimeMillis();
+		long totalVetor = fimVetor - inicioVetor;
 		
 		dijkstraVetor.imprimeCaminhoCompleto();
 		
+		System.out.println("O tempo total de processamento para este algoritmo foi de " + totalVetor + "ms.");
+		
 		DijkstraHeap dijkstraHeap = new DijkstraHeap(arquivo.getGrafo());
 
+		long inicioHeap = System.currentTimeMillis();
 		dijkstraHeap.obterArvoreCMC(0);
+		long fimHeap = System.currentTimeMillis();
+		long totalHeap = fimHeap - inicioHeap;
 		
 		dijkstraHeap.imprimeCaminhoCompleto();
+		
+		System.out.println("O tempo total de processamento para este algoritmo foi de " + totalHeap + "ms.");
 		
 	}
 	
