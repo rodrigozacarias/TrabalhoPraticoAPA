4e6eb27afad24deb0829325bf8832a5b750bcdee
alralves <alralves@firjan.com.br>
2021-07-30 22:14:09 -0300

    test run

--- TrabalhoDijkstra/src/DijkstraVetor/DijkstraVetor.java
+++ TrabalhoDijkstra/src/DijkstraVetor/DijkstraVetor.java
@@ -7,46 +7,48 @@ public class DijkstraVetor {
 	private Grafo grafo;
 	private double p[];
 	
+	public DijkstraVetor () {}
+
 	public DijkstraVetor (Grafo grafo) {
 		this.grafo = grafo;
 	}
 	
-	public void calculaMenorCaminho(int raiz) {
-			
-		int n = this.grafo.numVertices();
-		this.p = new double[n]; // peso dos vértices
+	public void calculaMenorCaminho(int raiz) { 
+		dijkstra(this.grafo,raiz);
+	}
+	
+	public void dijkstra(Grafo grafo,int raiz) {
+		
+		int n = grafo.numVertices();
+		this.p = new double[n]; //peso dos vértices
 		
         boolean visitados[] = new boolean[n];
-
         for (int i = 0; i < n; i++) {
             p[i] = Double.MAX_VALUE;
             visitados[i]=false;
         }
+        
         p[raiz] = 0;
-
+        
         for (int i = 0; i < n ; i++) {
-            int menorVertice = encontrarVerticeMinimo(p, visitados);
-            if(!this.grafo.listaAdjVazia(menorVertice)){
-				Grafo.Aresta adj = grafo.primeiroListaAdj(menorVertice);
+   /*1.*/   int menorVertice = encontrarVerticeMinimo(p, visitados);
+            if(!grafo.listaAdjVazia(menorVertice)){
+  /*2*/			Grafo.Aresta adj = grafo.primeiroListaAdj(menorVertice);
 				if( p[menorVertice] == Double.MAX_VALUE) {break;}
-				while(adj != null){
-					 	
-						if (adj.peso()  != 0 && !visitados[adj.v2()] ){
+				while(adj != null){					 	
+     				if (adj.peso()  != 0 && !visitados[adj.v2()] ){
 							if (p[adj.v2()] > p[menorVertice] + adj.peso()) {
 								p[adj.v2()] = p[menorVertice] + adj.peso();
-							}
-						
+							}						
 					 }
 					adj = grafo.proxAdj(menorVertice);
 				}
 				visitados[menorVertice] = true;
             }
         }
-
 	}
 	
 	private int encontrarVerticeMinimo(double[] distancias, boolean visitados[]) {
-
         int verticeMinimo = -1;
         for (int i = 0; i < distancias.length; i++) {
             if (!visitados[i] && (verticeMinimo == -1 || distancias[i] < distancias[verticeMinimo])) {
